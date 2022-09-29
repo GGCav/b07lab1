66b5e89eaafd85623d6b01fbe135b34f5353fb9c
GGCav <jeffreyhe406@gmail.com>
2022-09-29 16:45:23 -0400

    modifies Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,22 @@
+import java.io.*;
 public class Driver { 
 public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
+  Polynomial p = new Polynomial();  
   double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
+  Polynomial p1 = new Polynomial(c1);
+  System.out.println(p1.evaluate(3)); 
   double [] c2 = {0,-2,0,0,-9}; 
   Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
+  File f = new File("input.txt");
+  Polynomial p3 = new Polynomial(f);
+  System.out.println(p3.evaluate(1)); 
+  Polynomial s = p1.multiply(p2); 
+  p3.saveToFile("a.txt");
+  System.out.println("p(2) = " + p3.evaluate(2)); 
   if(s.hasRoot(1)) 
    System.out.println("1 is a root of s"); 
   else 
-   System.out.println("1 is not a root of s"); 
+   System.out.println("1 is not a root of s");
+
  } 
 } 
